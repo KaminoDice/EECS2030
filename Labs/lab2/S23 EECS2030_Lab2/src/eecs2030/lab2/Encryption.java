a06e385d1eda2535bd673a55b7c02554dde82c60
神の骰 <kaminodice@gmail.com>
2023-06-13 11:26:45 -0400

    Lab2 init
    lab2 import

---
+++ Labs/lab2/S23 EECS2030_Lab2/src/eecs2030/lab2/Encryption.java
@@ -0,0 +1,21 @@
+package eecs2030.lab2;
+
+/**
+ * Lab for reviewing array and String manipulation in Java and 
+ * an intro to utility classes and static features 
+ * 
+ * @author EECS2030
+ *
+ */
+public class Encryption {
+	private final static String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+	public static String encrypt(String textToEncrypt, String key) {
+		return textToEncrypt;
+	}
+
+	public static String decrypt(String textToDecrypt, String key) {
+		return textToDecrypt;
+	}
+
+}
