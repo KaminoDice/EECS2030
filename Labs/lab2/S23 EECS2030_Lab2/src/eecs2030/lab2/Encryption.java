94e9cfadce477c5d7f9552aa16419166bfb71ba2
神の骰 <kaminodice@gmail.com>
2023-06-14 23:28:31 -0400

    Lab2 finish

--- Labs/lab2/S23 EECS2030_Lab2/src/eecs2030/lab2/Encryption.java
+++ Labs/lab2/S23 EECS2030_Lab2/src/eecs2030/lab2/Encryption.java
@@ -4,51 +4,62 @@ import java.util.HashMap;
 import java.util.Random;
 
 /**
- * Lab for reviewing array and String manipulation in Java and 
- * an intro to utility classes and static features 
+ * Lab for reviewing array and String manipulation in Java and an intro to
+ * utility classes and static features
  * 
  * @author saikoro(Huanrui Cao/219256809)@EECS2030
  *
  */
 public class Encryption {
 	private final static String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-	
+
 	public static String encrypt(String textToEncrypt, String key) {
+
 		int hashSeed = key.hashCode();
 		HashMap<Character, Character> subsMap = hashMap(hashSeed);
-		
+		HashMap<Integer, Integer> transMap = hashTrans(hashSeed);
+
 		StringBuilder encryStr = new StringBuilder();
 		for (int i = 0; i < textToEncrypt.length(); i += 64) {
 			String chunk = textToEncrypt.substring(i, Math.min(i + 64, textToEncrypt.length()));
 			String subsChunk = toSubs(chunk, subsMap);
-			encryStr.append(toTrans(subsChunk, hashSeed));
-			//encryStr.append(subChunk);
-		}	
-		
+			encryStr.append(toTrans(subsChunk, transMap));
+		}
+
 		return encryStr.toString();
 	}
 
 	public static String decrypt(String textToDecrypt, String key) {
+
 		int hashSeed = key.hashCode();
 		HashMap<Character, Character> subsMap = hashMap(hashSeed);
 		HashMap<Character, Character> inveMap = new HashMap<Character, Character>();
 		subsMap.forEach((ori, subs) -> inveMap.put(subs, ori));
-		
-		
-		return textToDecrypt;
+
+		HashMap<Integer, Integer> transMap = hashTrans(hashSeed);
+		HashMap<Integer, Integer> revTrans = new HashMap<Integer, Integer>();
+		transMap.forEach((inde, trans) -> revTrans.put(trans, inde));
+
+		StringBuilder decryStr = new StringBuilder();
+		for (int i = 0; i < textToDecrypt.length(); i += 64) {
+			String chunk = textToDecrypt.substring(i, Math.min(i + 64, textToDecrypt.length()));
+			String revtransChunl = toTrans(chunk, revTrans);
+			decryStr.append(toSubs(revtransChunl, inveMap));
+		}
+
+		return decryStr.toString();
 	}
-	
-/*
-	public static void main(String[] args) {
-		System.out.println(encrypt(ALPHABET, "ABCDEFGH"));
-	}
-*/
-	
-	public static HashMap<Character, Character> hashMap(int hashSeed){
+
+	/*
+	 * public static void main(String[] args) { System.out.println(encrypt(ALPHABET,
+	 * "ABCDEFGH")); }
+	 */
+
+	public static HashMap<Character, Character> hashMap(int hashSeed) {
 		Random r = new Random(hashSeed);
 		StringBuilder subString = new StringBuilder(ALPHABET);
 		HashMap<Character, Character> subsMap = new HashMap<Character, Character>();
-		
+
 		for (int i = 0; i < 100; i++) {
 			int pos1 = r.nextInt(ALPHABET.length());
 			int pos2 = r.nextInt(ALPHABET.length());
@@ -56,37 +67,38 @@ public class Encryption {
 			subString.setCharAt(pos1, subString.charAt(pos2));
 			subString.setCharAt(pos2, pos1Char);
 		}
-		
+
 		for (int i = 0; i < ALPHABET.length(); i++) {
-			subsMap.put(ALPHABET.charAt(i),  subString.charAt(i));
+			subsMap.put(ALPHABET.charAt(i), subString.charAt(i));
 		}
-		
+
 		return subsMap;
-		
+
 	}
-	
+
 	public static String toSubs(String chunk, HashMap<Character, Character> subsMap) {
 		StringBuilder subsChunk = new StringBuilder();
-		//String coverChunk = String.format("%-64s", chunk);
-		
+		// String coverChunk = String.format("%-64s", chunk);
+
 		for (int i = 0; i < chunk.length(); i++) {
-			char subsChar = subsMap.getOrDefault(Character.toUpperCase(chunk.charAt(i)), subsMap.get(ALPHABET.charAt(ALPHABET.length()-1)));
+			char subsChar = subsMap.getOrDefault(Character.toUpperCase(chunk.charAt(i)),
+					subsMap.get(ALPHABET.charAt(ALPHABET.length() - 1)));
 			subsChunk.append(subsChar);
 		}
 
 		for (int i = chunk.length(); i < 64; i++) {
-			char subsChar = subsMap.get(ALPHABET.charAt(ALPHABET.length()-1));
+			char subsChar = subsMap.get(ALPHABET.charAt(ALPHABET.length() - 1));
 			subsChunk.append(subsChar);
 		}
 
 		return subsChunk.toString();
 	}
-	
-	
-	public static String toTrans(String subsChunk, int hashSeed) {
+
+	public static HashMap<Integer, Integer> hashTrans(int hashSeed) {
 		Random random = new Random(hashSeed);
-		int[] pattern = {0, 1, 2, 3, 4, 5, 6, 7};
-		
+		HashMap<Integer, Integer> transMap = new HashMap<Integer, Integer>();
+		int[] pattern = { 0, 1, 2, 3, 4, 5, 6, 7 };
+
 		for (int i = 0; i < 100; i++) {
 			int in1 = random.nextInt(pattern.length);
 			int in2 = random.nextInt(pattern.length);
@@ -94,23 +106,28 @@ public class Encryption {
 			pattern[in1] = pattern[in2];
 			pattern[in2] = tp;
 		}
-/*		
-		for (int i = 0; i < pattern.length; i++) {
-			System.out.println(pattern[i]);
-		}
-*/		
-		//System.out.println(subsChunk);
-		StringBuilder transChunk = new StringBuilder();
-		for (int i = 0; i < subsChunk.length(); i++) {
+
+		for (int i = 0; i < 64; i++) {
 			int col = pattern[i / pattern.length];
 			int row = i % pattern.length;
 			int index = col + row * pattern.length;
-			//System.out.println(index);
-			transChunk.append(subsChunk.charAt(index));
-			//System.out.println(transChunk.toString());
+			transMap.put(i, index);
 		}
-		
+		return transMap;
+	}
+
+	public static String toTrans(String subsChunk, HashMap<Integer, Integer> transMap) {
+
+		StringBuilder transChunk = new StringBuilder();
+
+		for (int i = 0; i < subsChunk.length(); i++) {
+			int traIndex = transMap.get(i);
+			// System.out.println(index);
+			transChunk.append(subsChunk.charAt(traIndex));
+			// System.out.println(transChunk.toString());
+		}
+
 		return transChunk.toString();
 	}
-	
+
 }
--- Labs/lab2/S23 EECS2030_Lab2/src/eecs2030/lab2/TestLab2.java
+++ Labs/lab2/S23 EECS2030_Lab2/src/eecs2030/lab2/TestLab2.java
@@ -2,13 +2,13 @@ package eecs2030.lab2;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
 import org.junit.runners.MethodSorters;
-
-import junit.framework.Assert;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestLab2 {
@@ -17,29 +17,25 @@ public class TestLab2 {
 	public Timeout globalTimeout = Timeout.seconds(2);
 
 	private static String encryptionKey = "ABCDEFGH";
-	private static String plainText = 
-			  "In cryptography, a substitution cipher is a method of encoding by "
+	private static String plainText = "In cryptography, a substitution cipher is a method of encoding by "
 			+ "which units of plaintext are replaced with ciphertext, according to a "
 			+ "fixed system; the \"units\" may be single letters (the most common), "
 			+ "pairs of letters, triplets of letters, mixtures of the above, and so "
-			+ "forth. The receiver deciphers the text by performing the inverse "
-			+ "substitution.";
+			+ "forth. The receiver deciphers the text by performing the inverse " + "substitution.";
 
-	private static String encryptedText = 
-			  "WGVYXOBJOTOR GOHYOYOJXUCT RJAFKUUSGYJOHBJBOJWVBWNXCBTAOS NVUOYAO"
+	private static String encryptedText = "WGVYXOBJOTOR GOHYOYOJXUCT RJAFKUUSGYJOHBJBOJWVBWNXCBTAOS NVUOYAO"
 			+ "XYGGGXYWZJLOLYTWOKATO OJJVJTWOABOU Y JAGNROEAZXOWOUAAWETXBYOHJYH"
 			+ "YAFRGUYAOEAOFJYXONXVAOOVBHOUNSAOSJYOOVAYUKVAOOLOOOOJOLTFGVYYCAVB"
 			+ "B OOYYJBWOKVAAFOUVYJKVTXFGLOVYEKOOBTLLOVYOOA OOAFJAYOAYOBTYTBTRY"
 			+ "CHYTOTAAGUTOTAY OOYJJXCTBOXAHVETOGBAAXOOAOKXP ANPVOWAOYKABOAWYOB"
 			+ "SPCBOOOOUURJOOOOAAYOOOOOOAVUOOOOJJVYOOOOFOOROOOOYTYOOOOOXVJOOOOO";
 
-	private static String decryptedText = 
-			    "IN CRYPTOGRAPHY  A SUBSTITUTION CIPHER IS A METHOD OF ENCODING B"
-			  + "Y WHICH UNITS OF PLAINTEXT ARE REPLACED WITH CIPHERTEXT  ACCORDI"
-			  + "NG TO A FIXED SYSTEM  THE  UNITS  MAY BE SINGLE LETTERS  THE MOS"
-			  + "T COMMON   PAIRS OF LETTERS  TRIPLETS OF LETTERS  MIXTURES OF TH"
-			  + "E ABOVE  AND SO FORTH  THE RECEIVER DECIPHERS THE TEXT BY PERFOR"
-			  + "MING THE INVERSE SUBSTITUTION                                   ";
+	private static String decryptedText = "IN CRYPTOGRAPHY  A SUBSTITUTION CIPHER IS A METHOD OF ENCODING B"
+			+ "Y WHICH UNITS OF PLAINTEXT ARE REPLACED WITH CIPHERTEXT  ACCORDI"
+			+ "NG TO A FIXED SYSTEM  THE  UNITS  MAY BE SINGLE LETTERS  THE MOS"
+			+ "T COMMON   PAIRS OF LETTERS  TRIPLETS OF LETTERS  MIXTURES OF TH"
+			+ "E ABOVE  AND SO FORTH  THE RECEIVER DECIPHERS THE TEXT BY PERFOR"
+			+ "MING THE INVERSE SUBSTITUTION                                   ";
 
 	@Test
 	public void test01_encrypt1() {
@@ -47,7 +43,7 @@ public class TestLab2 {
 		System.out.println("Expected:");
 		System.out.println(encryptedText);
 		assertTrue(Encryption.encrypt(plainText, encryptionKey).equals(encryptedText));
-		//assertEquals(Encryption.encrypt(plainText, encryptionKey), encryptedText);
+		// assertEquals(Encryption.encrypt(plainText, encryptionKey), encryptedText);
 	}
 
 	@Test
@@ -56,6 +52,43 @@ public class TestLab2 {
 		System.out.println("Expected:");
 		System.out.println(decryptedText);
 		assertTrue(Encryption.decrypt(encryptedText, encryptionKey).equals(decryptedText));
-		//assertEquals(Encryption.decrypt(encryptedText, encryptionKey), decryptedText);
+		// assertEquals(Encryption.decrypt(encryptedText, encryptionKey),
+		// decryptedText);
+	}
+
+	private static Random rng = new Random();
+	private static Random lengs = new Random();
+
+	private static char randomChar() {
+		return (char) (TestLab2.rng.nextInt('Z' - 'A') + 'A');
+	}
+
+	@Test
+	public void test03_randomatch() {
+		StringBuilder origText = new StringBuilder();
+		StringBuilder randomKey = new StringBuilder();
+
+		for (int i = 0; i < 128 + lengs.nextInt(128); i++) {
+			origText.append(TestLab2.randomChar());
+		}
+
+		System.out.println("OriginText: ");
+		System.out.println(origText.toString());
+
+		for (int i = 0; i < 4 + lengs.nextInt(60); i++) {
+			randomKey.append(TestLab2.randomChar());
+		}
+
+		System.out.println("GenerateKey: ");
+		System.out.println(randomKey.toString());
+		System.out.println("EncrypText: ");
+		String encryText = Encryption.encrypt(origText.toString(), randomKey.toString());
+		System.out.println(encryText);
+
+		System.out.println("DecrypText: ");
+		String decryText = Encryption.decrypt(encryText.toString(), randomKey.toString());
+		System.out.println(decryText);
+
+		assertEquals(String.format("%-1024s", origText), String.format("%-1024s", decryText));
 	}
 }
